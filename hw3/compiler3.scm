1d9139b3a3381bf8110cea23950a4dc5bfa77e91
avishagdaniely <avishagdaniely@gmail.com>
2016-12-19 12:00:52 -0800

    nested define - without define error check

--- hw3/compiler3.scm
+++ hw3/compiler3.scm
@@ -6,14 +6,14 @@
 		(letrec ((listDef (list))
 				(loop (lambda (exp rest)
 						(cond
-							((not (list? exp)) `(seq (,@listDef ,exp ,@rest))) 
+							((not (list? exp)) `(,listDef ,exp ,@rest))
 							((null? exp) listDef)
 							((null? rest) (if (equal? (car exp) 'def) 
 											(begin (set! listDef `(,@listDef (set ,(cadr exp) ,(caddr exp)))) listDef)
-											`(seq (,@listDef ,exp))))
+											`(,listDef ,exp)))
 							((equal? (car exp) 'def) (begin (set! listDef `(,@listDef (set ,(cadr exp) ,(caddr exp)))) (loop (car rest) (cdr rest))))
-							(else `(seq (,@listDef ,exp ,@rest)))
-				))))
+							(else `(,listDef ,exp ,@rest)))
+				)))
 				(loop exp rest)
 	)))
 
@@ -21,8 +21,28 @@
 (define validSeq?
 	(lambda (exp)
 		(andmap (lambda (shit) (not (equal? (car shit) 'def))) exp)
-	)
-)
+	))
+
+(define varsGetter
+	(lambda (exp)
+		(map (lambda (exp2) (cadadr exp2)) exp)))
+
+(define falseMaker
+	(lambda (exp)
+		(map (lambda (exp2) '(const #f)) exp)))
+
+
+
+(define noDefines
+	(lambda (exp)
+		(letrec ((isValidNoDefList #t)
+				(loop (lambda (exp2)
+						(cond 
+							((not (list? exp)) isValidNoDefList)
+							((null? exp) isValidNoDefList)
+							((equal? (car exp) 'def) (set! isValidNoDefList #f))
+							(else (loop (car exp)) (loop (cdr exp)))))))
+		(loop exp) isValidNoDefList)))
 
 
 (define inLambda 
@@ -35,11 +55,17 @@
 				;;;;seq
 				(pattern-rule
 				`(seq ,(? 'exp))
-					(lambda (exp)  (let ((defExp (defGetter (car exp) (cdr exp))))
-												(if (validSeq? (cadr defExp))
-													defExp
-													(error 'inLambda "Seq in not a valid seq: ~s" )))
-					)) 
+					(lambda (exp)  
+						(let ((defExp (defGetter (car exp) (cdr exp))))
+								(if (validSeq? (car defExp))
+									(let* ((vars (varsGetter (car defExp)))
+											(falseLst (falseMaker vars)))
+											`(applic (lambda-simple ,vars (seq (,@(eliminate-nested-defines-helper (car defExp)) ,@(eliminate-nested-defines-helper (cdr defExp)))))
+												,falseLst))
+													(error 'inLambda "Seq in not a valid seq: ~s" )))))
+				(pattern-rule
+				(? 'exp)
+					(lambda (exp)  (eliminate-nested-defines-helper exp)))
 		)))  
 			(lambda (e)
 				(run e
@@ -48,7 +74,7 @@
 									(format "I can't recognize this: ~s" e)))))))
 
 
-(define eliminate-nested-defines
+(define eliminate-nested-defines-helper
 	(let ((run 
 			(compose-patterns
 				;;;;lambda
@@ -67,11 +93,8 @@
 						(cond
 							((null? exp) exp) 
 							((not (list? exp)) exp)
-							((list? (car exp)) `(,(eliminate-nested-defines (car exp)) ,@(eliminate-nested-defines (cdr exp))))
-							(else `(,(car exp) ,@(eliminate-nested-defines (cdr exp)))))
-						))
-
-
+							((list? (car exp)) `(,(eliminate-nested-defines-helper (car exp)) ,@(eliminate-nested-defines-helper (cdr exp))))
+							(else `(,(car exp) ,@(eliminate-nested-defines-helper (cdr exp)))))))
 		)))
 			(lambda (e)
 				(run e
@@ -80,9 +103,14 @@
 									(format "I can't recognize this: ~s" e)))))))
 
 
-
-
-
+(define eliminate-nested-defines
+	(lambda (exp)
+		(let ((noNestedDefines (eliminate-nested-defines-helper exp)))
+			;(if (noDefines (cdr noNestedDefines))
+				noNestedDefines
+				;(error 'define "inapropriate defines ~s" ))
+			)
+		))
 
 
 (eliminate-nested-defines '(def (var my-even?)
@@ -103,5 +131,5 @@
 (var even?)
 ((applic (var -) ((var n) (const 1)))))
 (const #f))))
-(applic (var even?) ((var e))))))))
+(applic (var even?) ((var e)))))))))
 
