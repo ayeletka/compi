a887f042e5d2b675ea60785839b0de650c633183
avishagdaniely <avishagdaniely@gmail.com>
2016-12-23 14:55:46 -0800

    a few tests + beggining of Annotating Variables with their Lexical address

--- hw3/tests.scm
+++ hw3/tests.scm
@@ -2,15 +2,16 @@
 (load "compiler3.scm")
 
 (define test1 '(lambda a (+ 1 2))) ;basic lambda var
-(define test2 '(lambda (a b . c) (begin (define d a) (begin (+ 1 2))))) ;does not pass eliminate-nested-defines
-(define test3 '(lambda a (lambda () (begin (define d a) (begin (+ 1 2))))))
-(define test4 '(lambda a (letrec ((a b) (c d)) (lambda (a) c))))
+(define test2 '(lambda (a b . c) (begin (define d a) (begin (+ 1 2))))) 
+(define test3 '(lambda a (lambda () (begin (define d a) (begin (+ 1 2)))))) ;fails on boxingOfVariables - on cdr
+(define test4 '(lambda a (letrec ((a b) (c d)) (lambda (a) a))))	;fails on boxingOfVariables - error does not run right + rename to box-set as stated in assaignment)
 (define *test-expr* '(define my-even? (lambda (e) (define even? (lambda () (or (zero? n) (odd? (- n 1))))) (define odd? (lambda (n) (and (positive? n) (even? (- n 1))))) (even? e))))
+(define *example* '(let ((a 0)) (list (lambda () a) (lambda () (set! a (+ a 1))) (lambda (b) (set! a b)))))
 
 
 
 (display "\n")	
-(define test-parsed (parse test4))
+(define test-parsed (parse test3))
 
 (display test-parsed)
 
@@ -22,4 +23,10 @@
 
 (display "\n")
 
-(remove-applic-lambda-nil end)
+(define raln (remove-applic-lambda-nil end))
+
+(display "\n")
+
+(display raln)
+
+(boxingOfVariables raln)
