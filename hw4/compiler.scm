17abd5a6a9547f4275d43977aff34e5731412998
Ayelet Kalderon <kalderonayelet@gmail.com>
2017-02-21 23:54:45 +0200

    mul with fraction

--- hw4/compiler.scm
+++ hw4/compiler.scm
@@ -241,6 +241,10 @@
                   "MOV(IND(" (number->string idx) "), R0);" nl )))
       ((equal? var '-) (string-append (closureFromLabelMaker "MINUS") (string-append 
                   "MOV(IND(" (number->string idx) "), R0);" nl )))
+    ;  ((equal? var '/) (string-append (closureFromLabelMaker "DIVIDE") (string-append 
+     ;             "MOV(IND(" (number->string idx) "), R0);" nl )))
+      ((equal? var '*) (string-append (closureFromLabelMaker "MULTIPLY") (string-append 
+                  "MOV(IND(" (number->string idx) "), R0);" nl )))
       ;((equal? var 'cons) (string-append (closureFromLabelMaker "CONS") (string-append 
       ;            "MOV(IND(" (number->string idx) "), R0);" nl )))
       ;((equal? var 'list) (string-append (closureFromLabelMaker "LIST") (string-append 
